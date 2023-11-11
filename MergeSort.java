6e0402e8a84e7de94193993ac6800ac263acedac
Nikita-Savel <n89227567430@yandex.ru>
2023-11-11 17:53:55 +0300

    Homework_1_3

--- MergeSort.java
+++ MergeSort.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Merge_Sort{ 
+public class MergeSort{ 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String[] input = reader.readLine().split(" ");
